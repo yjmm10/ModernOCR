83429b69d24d062873be8c0bba0be2f29b78289f
Petrichor <yjmm10@yeah.net>
2022-03-07 17:54:24 +0800

    Update build.bat

--- script/build.bat
+++ script/build.bat
@@ -1,1 +1,1 @@
-mkdir build | cd build && cmake -G "Visual Studio 16 2019" -A "Win32" .. && cmake --build . --config Release && cmake --install . --prefix install
+mkdir build || cd build && cmake -G "Visual Studio 16 2019" -A "Win32" .. && cmake --build . --config Release && cmake --install . --prefix install
