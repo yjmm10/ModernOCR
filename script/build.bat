e0079d554a96891f88c19179edb9ffc6d00a7d08
Petrichor <yjmm10@yeah.net>
2022-03-08 20:36:15 +0800

    修改编译程序

--- script/build.bat
+++ script/build.bat
@@ -1,1 +1,2 @@
 mkdir build || cd build && cmake -G "Visual Studio 16 2019" -A "Win32" .. && cmake --build . --config Release && cmake --install . --prefix install
+@REM mkdir build || cd build && cmake -G "Visual Studio 16 2019" -A "Win32" .. -DBUILD_TEST=ON && cmake --build . --config Release && cmake --install . --prefix install
