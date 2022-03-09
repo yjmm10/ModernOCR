5153ac0c1ae0086690f8217f32f46f81bb0bd3fb
Petrichor <yjmm10@yeah.net>
2022-03-09 18:04:13 +0800

    Update build.bat

--- script/build.bat
+++ script/build.bat
@@ -1,2 +1,3 @@
-mkdir build || cd build && cmake -G "Visual Studio 16 2019" -A "Win32" .. && cmake --build . --config Release && cmake --install . --prefix install
+@REM mkdir build || cd build && cmake -G "Visual Studio 16 2019" -A "Win32" .. && cmake --build . --config Release && cmake --install . --prefix install
+mkdir build || cd build && cmake --build . --config Release && cmake --install . --prefix install
 @REM mkdir build || cd build && cmake -G "Visual Studio 16 2019" -A "Win32" .. -DBUILD_TEST=ON && cmake --build . --config Release && cmake --install . --prefix install
